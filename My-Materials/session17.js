5922130520619b480290f7962a9d1b8cc2c8f811
shymaagamal <shymaagamal666@gmail.com>
2024-08-13 14:55:38 +0300

    session 17

---
+++ My-Materials/session17.js
@@ -0,0 +1,16 @@
+console.log(x)
+var x=10
+
+function sum(operator,...res){
+    if(operator == "M")
+    {
+
+    }
+    else if()   
+    console.log(res)
+    // var x=10
+}
+
+
+
+sum(10,20,30,40,50)
